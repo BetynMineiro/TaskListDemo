3b421a34094ef1140aab169a34407491933769d4
Carlos Silva <calberto@brainworks.com>
2025-01-27 23:29:33 -0300

    Update logger class references in Bus.cs
    Replaced outdated "DNTL.Task.CrossCutting.Bus" references with "Task.CrossCutting.Bus" in logger messages to ensure consistency and accuracy in logging information. This improves clarity and aligns with the current namespace structure.

--- Task.CrossCutting/Bus/Bus.cs
+++ Task.CrossCutting/Bus/Bus.cs
@@ -7,21 +7,21 @@ public class Bus(IMediator mediator, ILogger logger) : IBus
 {
     public async Task<object> SendCommandAsync<TCommand>(TCommand command, CancellationToken cancellationToken)
     {
-        logger.Information("Calling Command Bus | Class: DNTL.Task.CrossCutting.Bus | Method: SendCommandAsync");
+        logger.Information("Calling Command Bus | Class: Task.CrossCutting.Bus | Method: SendCommandAsync");
 
         return await mediator.Send(command, cancellationToken).ConfigureAwait(false);
     }
 
     public async System.Threading.Tasks.Task SendEventAsync<TEvent>(TEvent @event, CancellationToken cancellationToken)
     {
-        logger.Information("Calling Event Bus | Class: DNTL.Task.CrossCutting.Bus | Method: SendEventAsync");
+        logger.Information("Calling Event Bus | Class: Task.CrossCutting.Bus | Method: SendEventAsync");
 
         await mediator.Publish(@event, cancellationToken).ConfigureAwait(false);
     }
 
     public async Task<object> SendQueryAsync<TQuery>(TQuery query, CancellationToken cancellationToken)
     {
-        logger.Information("Calling Query Bus | Class: DNTL.Task.CrossCutting.Bus | Method: SendQueryAsync");
+        logger.Information("Calling Query Bus | Class: Task.CrossCutting.Bus | Method: SendQueryAsync");
 
         return await mediator.Send(query, cancellationToken).ConfigureAwait(false);
     }
